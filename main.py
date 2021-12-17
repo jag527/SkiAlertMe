a0fdce9ca30cc10d463939f0c0d9a315172c41f7
Jonathan Grossman <jag527@cornell.edu>
2021-12-17 15:46:29 -0500

    Update main.py
    Trying to import urllib

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-def url_maker(resort_id, app_id, key):
-    base = "api.weatherunlocked.com/api/resortforecast/"
-    resort_id = str(resort_id)
-    app_id = str(app_id)
-    key = str(key)
-
-    url = base+resort_id+"?app_id="+app_id+"&app_key="+key
-    return url
+import urllib.request
 
 
-def setup_api():
-    app_id = 87516043
-    key = "82c84db08480af06fbe40d3a85190a0a"
-    accept_header = "application/json"
-    resort_id = 802019
-    url = url_maker(resort_id, app_id, key)
+class SkiAlertMe:
+    def __init__(self):
+        self.app_id = 87516043
+        self.key = "82c84db08480af06fbe40d3a85190a0a"
+        self.accept_header = "application/json"
+        self.resort_id = 802019  # Stratton
+
+    def url_maker(self):
+        base = "api.weatherunlocked.com/api/resortforecast/"
+        resort_id = str(self.resort_id)
+        app_id = str(self.app_id)
+        key = str(self.key)
+        url = base+resort_id+"?app_id="+app_id+"&app_key="+key
+        return url
+
+    def read_file(self, url):
+        with urllib.request.urlopen(url) as f:
+            print(f)
 
 
 def run():
-    setup_api()
-    # test
+    alert = SkiAlertMe()
+    url = alert.url_maker()
+    alert.readFile(url)
 
 
 if __name__ == "__main__":
