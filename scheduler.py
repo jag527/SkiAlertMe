db681c938f9b42c25655185cf9c9fbbec977d0ea
Jonathan Grossman <jag527@cornell.edu>
2021-12-18 11:00:14 -0500

    Adding in scheduling

---
+++ scheduler.py
@@ -0,0 +1,15 @@
+# import time
+# import schedule
+# import send_email
+#
+#
+# def send():
+#     send_email.run()
+#
+#
+# schedule.every().day.at("11:00").do(send())
+#
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
--- send_email.py
+++ send_email.py
@@ -2,42 +2,49 @@ import smtplib
 import main
 
 
+email_list = ["XXXXX@gmail.com",
+              "XXXXX@gmail.com"]
+
+
 def d_to_message(d):
-    intro = "Report for " + d[0] + ": \n"
+    intro = "Report for " + d[0] + ": \n \n"
     day1 = "On " + str(d[1]["Day"]) + " there will be " + \
            str(d[1]["Total Snowfall"]) + " in. of total snowfall, with " + \
            d[1]["Morning Weather"].lower() + ", " +\
            d[1]["Midday Weather"].lower() + ", and " +\
            d[1]["Night Weather"].lower() + " " \
-           + "weather throughout the day. \n"
+           + "weather throughout the day. \n \n"
     day2 = "On " + str(d[2]["Day"]) + " there will be " + \
            str(d[2]["Total Snowfall"]) + " in. of total snowfall, with " + \
            d[2]["Morning Weather"].lower() + ", " + \
            d[2]["Midday Weather"].lower() + ", and " +\
            d[2]["Night Weather"].lower() + " " + \
-           "weather throughout the day. \n"
+           "weather throughout the day. \n \n"
     conclude = "See more at " + d[3]
-    return intro + day1 + day2 + conclude
+    message = "\n" + intro + day1 + day2 + conclude
+    return message
 
 
-def send_email(d=""):
+def send_email(d, lst):
     # takes in a dictionary created in main.py
     # sends email with the info from the dictionary
+    # sends emails to all addresses in lst
 
     server = smtplib.SMTP_SSL("smtp.gmail.com")
-    server.login("XXXX@gmail.com",
-                 "XXXXX")
+    server.login("XXXXX@gmail.com",
+                 "xxxxx")
     message = d_to_message(d)
-    print(message)
-    server.sendmail("dailystrattonupdate@gmail.com",
-                    "jgrossman1919@yahoo.com",
-                    message)
+
+    for address in lst:
+        server.sendmail("dailystrattonupdate@gmail.com",
+                        address, message)
+
     server.quit()
 
 
 def run():
-    d = main.run()
-    send_email(d)
+    weather_dict = main.run()
+    send_email(weather_dict, email_list)
 
 
 if __name__ == "__main__":
